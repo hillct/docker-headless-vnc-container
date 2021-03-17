24f9833aa15d85cc5b5d38ba845c2f414ff3e36d
Colin Hill <hillct@gmail.com>
2021-03-17 16:37:26 -0400

    Added Script for Chrome insallation - no browsers installed by default    -- Kept scripts for Chromium and Firefox

---
+++ src/ubuntu/install/google-chrome.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
+# Direct Download: https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+echo "Install Google Chrome Browser"
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+apt-get update -y
+apt-get install -y fonts-liberation google-chrome-stable
+apt autoremove -y
